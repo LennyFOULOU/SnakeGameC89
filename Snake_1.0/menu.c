794f657d9f53ab0ad4fc523fedde73c8db37ec4e
groll <theo.groll@etu.u-pec.fr>
2023-12-23 17:05:02 +0100

    Ecran de démarrage qualitatif

--- Snake_1.0/menu.c
+++ Snake_1.0/menu.c
@@ -7,19 +7,31 @@
 
 
 void AfficherMenu() {
-ChargerImage("image/demarrage.png", (LARGEUR_FENETRE - 1300) / 2, (HAUTEUR_FENETRE - 900) / 2, 0, 0, 1000, 1000);
+  ChargerImage("image/demarrage.png", 0, 50, 0, 0, 1600, 900);
   AfficherFenetre();
+  RemplirRectangle(0, HAUTEUR_FENETRE - 80, LARGEUR_FENETRE, 80);
+  RemplirRectangle(0, 0, LARGEUR_FENETRE, 50);
 }
 
-    int GererMenu(void) {
+int GererMenu(void) {
     int x, y;
-   while(1) {
-    SourisPosition();
-    while(!SourisCliquee()) {
-      AfficherMenu();
-    }
-    return 1;
-   }
-return 0;
-}
+    while(1) {
+        SourisPosition();
+        while(!SourisCliquee()) {
+            AfficherMenu();
+        }
+        x = _X;
+        y = _Y;
 
+        /* Vérification du clic sur le premier bouton pour lancer le jeu */
+        if (x >= 90 && x <= 505 && y >= 390 && y <= 520) {
+	  return 1; /* Retourne 1 pour indiquer le démarrage du jeu */
+        }
+
+        /* Vérification du clic sur le second bouton pour quitter la fenêtre */
+        if (x >= 90 && x <= 505 && y >= 620 && y <= 750) {
+	  return 0; /* Retourne 0 pour quitter la fenêtre */
+        }
+    }
+    return 0;
+}
